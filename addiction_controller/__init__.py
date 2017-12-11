9a73b73b87a30697e56d9e284833b00028015d73
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-12-11 09:04:24 +0500

    Added: heroku environment port autodetection

--- addiction_controller/__init__.py
+++ addiction_controller/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from addiction_controller.model import data
 
@@ -10,4 +11,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
